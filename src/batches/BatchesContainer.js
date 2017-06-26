0eb506725bc006d8242206e7156db0ed393505b0
annmagdalen <annmagdalen@hotmail.com>
2017-06-26 19:40:28 +0200

    Connected API

--- src/batches/BatchesContainer.js
+++ src/batches/BatchesContainer.js
@@ -1,9 +1,20 @@
 import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import Title from '../components/Title'
 import BatchesItem from './BatchesItem'
+import fetchBatches from '../actions/batches/fetch'
 
 class BatchesContainer extends PureComponent {
+  static propTypes = {
+    batches: PropTypes.array.isRequired,
+    fetchBatches: PropTypes.func.isRequired,
+  }
+
+  componentWillMount() {
+    this.props.fetchBatches()
+  }
+
   renderBatch(batch, index) {
     return <BatchesItem key={index} { ...batch } />
   }
@@ -27,4 +38,4 @@ const mapStateToProps = ({ batches }) => ({
   batches
 })
 
-export default connect(mapStateToProps)(BatchesContainer)
+export default connect(mapStateToProps, { fetchBatches })(BatchesContainer)
--- src/reducers/batches.js
+++ src/reducers/batches.js
@@ -1,9 +1,13 @@
 import { CREATE_BATCH } from '../actions/batches/create'
+import { FETCHED_BATCHES } from '../actions/batches/fetch'
 
 export default (state = [], { type, payload } = {}) => {
   switch(type) {
     case CREATE_BATCH :
       return [Object.assign({}, payload)].concat(state)
+
+    case FETCHED_BATCHES :
+      return [].concat(payload)
 
     default :
       return state
